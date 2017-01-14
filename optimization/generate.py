ad48f25fbca1a0e89c2e958bd5a2be226e1cfe31
Edward Zhang <edzhang@cs.washington.edu>
2017-01-13 23:52:02 -0800

    Generate synthetic datasets

---
+++ optimization/generate.py
@@ -0,0 +1,37 @@
+from random import uniform
+
+nlights = [2,3,4,5]
+sep = 0.05
+ncases = 100
+filename = "box%03d.lt"
+
+scene = [
+"-1 -1.01 -1 1.01",
+"-1.01 1 1.01 1",
+"1 1.01 1 -1.01 ",
+"1.01 -1 -1.01 -1"
+]
+
+def dist2(a,b):
+    return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1])
+
+def genlights(n):
+    lights = []
+    valid = False
+    while not valid:
+        lights = [(uniform(-1,1), uniform(-1,1), uniform(1,10)) for i in range(n)]
+        valid = True
+        for i in range(len(lights)):
+            for j in range(i+1,len(lights)):
+                if dist2(lights[i], lights[j]) < sep*sep:
+                    valid = False
+        return ["%f %f %f"%l for l in lights]
+
+
+for n in nlights:
+    for i in range(ncases):
+        f = open(filename%(n*ncases+i), "w")
+        f.write("4 %d\n"%n);
+        f.writelines('\n'.join(scene))
+        f.write("\n")
+        f.writelines('\n'.join(genlights(n)))
